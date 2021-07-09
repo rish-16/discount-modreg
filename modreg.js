7d49998e82b4e3d5a1db65f27430abbf69cef49a
rish-16 <mail.rishabh.anand@gmail.com>
2021-07-09 22:12:45 +0800

    Add panel shuffle on ModReg

--- modreg.js
+++ modreg.js
@@ -23,12 +23,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function switchPanel(idx) {
         var panels = [
-            aboutPanel, viewClassesPanel, modSelectPanel, modReqPanel, selectTutPanel, addSwapPanel, dropPanel, appealsPanel
+            aboutPanel, 
+            viewClassesPanel,
+            modSelectPanel,
+            modReqPanel,
+            selectTutPanel,
+            addSwapPanel,
+            dropPanel,
+            appealsPanel
         ]
 
-        for (panel in panels) {
-            panel.style.display = 'none'
+        for (var i=0, item; item = panels[i]; i++) {
+            // Look no need to do list[i] in the body of the loop
+            // console.log("Looping: index ", i, "item" + item);
+            item.style.display = 'none'
         }
+
+        console.log(`Showing panel ${idx}.`)
 
         panels[idx].style.display = 'block'
     }
@@ -36,40 +47,48 @@ document.addEventListener('DOMContentLoaded', () => {
     aboutBtn.addEventListener('click', () => {
         console.log('About clicked')
         pageTitle.innerText = 'About ModReg@EduRec'
+        switchPanel(0)
     })
 
     viewClassesBtn.addEventListener('click', () => {
         console.log('View Classes clicked')
         pageTitle.innerText = 'View My Classes@ModReg'
+        switchPanel(1)
     })
 
     modSelectBtn.addEventListener('click', () => {
         console.log('Module Select clicked')
         pageTitle.innerText = 'Select Modules'
+        switchPanel(2)
     })
 
     modReqBtn.addEventListener('click', () => {
         console.log('Mod Requests clicked')
         pageTitle.innerText = 'Submit Module Requests'
+        switchPanel(3)
     })
 
     selectTutBtn.addEventListener('click', () => {
         console.log('Select Tutorials clicked')
         pageTitle.innerText = 'Select Tutorials/Labs'
+        switchPanel(4)
     })
 
     addSwapBtn.addEventListener('click', () => {
         console.log('Add Swap clicked')
         pageTitle.innerText = 'Add/Swap Tutorials/Labs'
+        switchPanel(5)
     })
 
     dropBtn.addEventListener('click', () => {
         console.log('Drop clicked')
         pageTitle.innerText = 'Drop Classes'
+        switchPanel(6)
     })
 
     appealsBtn.addEventListener('click', () => {
         console.log('Appeals clicked')
         pageTitle.innerText = 'Submit Appeals/Inquiries'
+        switchPanel(7)
     })
 })

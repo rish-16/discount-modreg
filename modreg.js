bdb6a0a953e2f1d543d72850d7767ddbe244f3df
rish-16 <mail.rishabh.anand@gmail.com>
2021-07-13 04:34:26 +0800

    Add search, module table updates, module querying

--- modreg.js
+++ modreg.js
@@ -1,3 +1,254 @@
+const ALLMODS = `{
+    "CS1101S": {
+        "name": "CS1101S",
+        "detailed-name": "Programming Methodology I",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS2040S": {
+        "name": "CS2040S",
+        "detailed-name": "Data Structures and Algorithms",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "BT1101": {
+        "name": "BT1101",
+        "detailed-name": "Introduction to Business Analytics",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS1010S": {
+        "name": "CS1010S",
+        "detailed-name": "Programming Methodology",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "MKT1705X": {
+        "name": "MKT1705X",
+        "detailed-name": "Principles of Marketing",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "EC1101E": {
+        "name": "EC1101E",
+        "detailed-name": "Introduction to Economic Analysis",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "BT2101": {
+        "name": "BT2101",
+        "detailed-name": "Econometrics Modeling for Business Analytics And/Or",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "BT2102": {
+        "name": "BT2102",
+        "detailed-name": "Data Management and Visualisation",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS2030": {
+        "name": "CS2030",
+        "detailed-name": "Programming Methodology II",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS1010J": {
+        "name": "CS1010J",
+        "detailed-name": "Programming Methodology",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS1231": {
+        "name": "CS1231",
+        "detailed-name": "Discrete Structures ",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "MA1521": {
+        "name": "MA1521",
+        "detailed-name": "Calculus for Computing ",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "IS2101": {
+        "name": "IS2101",
+        "detailed-name": "Business and Technical Communication",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS2040": {
+        "name": "CS2040",
+        "detailed-name": "Data Structures and Algorithm ",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "IS1103": {
+        "name": "IS1103",
+        "detailed-name": "Ethics in Computing",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS1231S": {
+        "name": "CS1231S",
+        "detailed-name": "Discrete Structures",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "MA2001": {
+        "name": "MA2001",
+        "detailed-name": "Linear Algebra I",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS2040C": {
+        "name": "CS2040C",
+        "detailed-name": "Data Structures & Algorithms",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS2100": {
+        "name": "CS2100",
+        "detailed-name": "Computer Organisation",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CS1010": {
+        "name": "CS1010",
+        "detailed-name": "Programming Methodology",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CG1111A": {
+        "name": "CG1111A",
+        "detailed-name": "Engineering Principles and Practice I",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "MA1511": {
+        "name": "MA1511",
+        "detailed-name": "Engineering Calculus",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "MA1512": {
+        "name": "MA1512",
+        "detailed-name": "Differential Equations for Enginneering",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "EG1311": {
+        "name": "EG1311",
+        "detailed-name": "Design And Make (New)",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "CG2111A": {
+        "name": "CG2111A",
+        "detailed-name": "Engineering Principles and Practice II",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "DTK1234": {
+        "name": "DTK1234",
+        "detailed-name": "Design Thinking (New)",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "PF1101": {
+        "name": "PF1101",
+        "detailed-name": "Fundamentals of Project Management (New)",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "GEA1000": {
+        "name": "GEA1000",
+        "detailed-name": "Quantitative Reasoning with Data",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "MA1508E": {
+        "name": "MA1508E",
+        "detailed-name": "Linear Algebra for Engineering",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "GER1000": {
+        "name": "GER1000",
+        "detailed-name": "Quantitative Reasoning",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    },
+    "GEH1026": {
+        "name": "GEH1026",
+        "detailed-name": "Living with Mathematics",
+        "vacancy": "600",
+        "occupancy": 153,
+        "lecture-id": "L-L1-1226",
+        "units": "4.00"
+    }
+}`
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('App loaded.')
 
@@ -27,17 +278,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchModuleBtn = document.getElementById('modsel-search')
     const modContainerTable = document.getElementById('modsel-final-mods-container')
 
-    // const mcSetter = document.getElementById('modsel-mc-setter')
-    // var curMCLimit = 0;
-    // 
-    // mcSetter.addEventListener('click', () => {
-        // curMCLimit = mcSetter.innerText;
-        // console.log(curMCLimit)
-    // })
+    const popupResultSubjectInput = document.getElementById('modsel-container-subject-input')
+    const popupResultNbrInput = document.getElementById('modsel-container-catalog-nbr-input')
+    const popupResultModTitleInput = document.getElementById('modsel-container-subject-mod-title-input')
+    const popupResultBannerTitle = document.getElementById('mod-result-title')
+    const modResultsTable = document.getElementById('modsel-search-result-container')
+
+    var curRank = 1 // maintains next highest rank in Table
 
     // load the about panel on page load
     pageTitle.innerText = 'About ModReg@EduRec'
-    modSelectPanel.style.display = 'block'
+    aboutPanel.style.display = 'block'
 
     function switchPanel(idx) {
         var panels = [
@@ -113,6 +364,10 @@ document.addEventListener('DOMContentLoaded', () => {
     addClassBtn.addEventListener('click', () => {
         console.log('Adding class')
         modSelPopup.style.display = 'block'
+
+        popupResultSubjectInput.value = ''
+        popupResultNbrInput.value = ''
+        popupResultModTitleInput.value = ''
     })
 
     modSelPopupClose.addEventListener('click', () => {
@@ -157,14 +412,104 @@ document.addEventListener('DOMContentLoaded', () => {
 
         modRemove.addEventListener('click', () => {
             modContainerTable.removeChild(mod)
+            curRank -= 1
         })
 
         modContainerTable.appendChild(mod)
     }
 
+    function addModToResults(details) {
+        var mod = document.createElement('div')
+        mod.classList += 'mod-res'
+
+        var modClass = document.createElement('p')
+        modClass.innerText = details['lecture-id']
+        modClass.classList += 'mod-res-class'
+
+        var modActivity = document.createElement('p')
+        modActivity.innerText = 'Lecture'
+        modActivity.classList += 'mod-res-activity'
+
+        var modSession = document.createElement('p')
+        modSession.innerText = 'Regular'
+        modSession.classList += 'mod-res-session'
+        
+        var modVacancy = document.createElement('p')
+        modVacancy.innerText = details['vacancy']
+        modVacancy.classList += 'mod-res-vacancy'
+        
+        var modOcc = document.createElement('p')
+        modOcc.innerText = details['occupancy']
+        modOcc.classList += 'mod-res-selected'
+
+        var modPlaceholder = document.createElement('p')
+        modPlaceholder.classList += 'mod-res-select'
+
+        var selectBtn = document.createElement('button')
+        selectBtn.innerHTML = 'Select'
+        selectBtn.classList += 'mod-res-select-btn'
+        modPlaceholder.appendChild(selectBtn)
+
+        selectBtn.addEventListener('click', () => {
+            modSelPopup.style.display = 'none' // hide search popup
+            addModToTable(details['detailed-name'], details['units'], curRank)
+            curRank += 1
+
+            console.log('flushing')
+
+            // flush
+            modResultsTable.innerHTML = `
+            <div id="modsel-search-result-header">
+                <p style="width: 20%; font-weight: bold;">Class</p>
+                <p style="width: 20%; font-weight: bold;">Activity</p>
+                <p style="width: 10%; font-weight: bold;">Session</p>
+                <p style="width: 10%; font-weight: bold;">Vacancy</p>
+                <p style="width: 25%; font-weight: bold;">Students Selected</p>
+                <p style="width: 15%; font-weight: bold;"></p>
+            </div>`
+
+            popupResultSubjectInput.value = ''
+            popupResultNbrInput.value = ''
+            popupResultModTitleInput.value = ''
+
+            popupResultBannerTitle.innerHTML = ''
+        })
+
+        mod.appendChild(modClass)
+        mod.appendChild(modActivity)
+        mod.appendChild(modSession)
+        mod.appendChild(modVacancy)
+        mod.appendChild(modOcc)
+        mod.appendChild(modPlaceholder)
+
+        modResultsTable.appendChild(mod)
+    }
+
+    function searchMods(key) {
+        const allmods = JSON.parse(ALLMODS)
+        console.log(allmods)
+
+        if (allmods[key] != undefined) {
+            // module found
+            return allmods[key]
+        } else {
+            popupResultBannerTitle.innerHTML = `Module ${key} not found. Try again with another search query.`
+        }
+    }
+
     searchModuleBtn.addEventListener('click', () => {
-        console.log('Closing popup')
-        modSelPopup.style.display = 'none'
-        addModToTable('CS1231 Discrete Structures', 4.00, 3)
+        console.log('Searching')
+        // get the results from the text field
+        var moduleCode = popupResultSubjectInput.value
+        console.log(moduleCode)
+
+        popupResultBannerTitle.innerHTML = `Select a Class for Module ${moduleCode} <i class="fas fa-info-circle" style="color: #54a0ff; padding-left: 5px;"></i>`
+
+        // search for the module code and get details
+        var details = searchMods(moduleCode)
+
+        // make a row for the module in results
+        // on select, render row in main mod table
+        addModToResults(details)
     })
 })

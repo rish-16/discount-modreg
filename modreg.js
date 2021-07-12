61644c67c4fd3aaf1b4053128e3bafd789eeac39
rish-16 <mail.rishabh.anand@gmail.com>
2021-07-13 03:05:54 +0800

    Add module selection popup

--- modreg.js
+++ modreg.js
@@ -21,6 +21,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const pageTitle = document.getElementById('page-title')
 
+    const addClassBtn = document.getElementById('modsel-submit')
+    const modSelPopup = document.getElementById('modsel-select-popup')
+    const modSelPopupClose = document.getElementById('modsel-popup-close')
+    const searchModuleBtn = document.getElementById('modsel-search')
+    const modContainerTable = document.getElementById('modsel-final-mods-container')
+
+    // const mcSetter = document.getElementById('modsel-mc-setter')
+    // var curMCLimit = 0;
+    // 
+    // mcSetter.addEventListener('click', () => {
+        // curMCLimit = mcSetter.innerText;
+        // console.log(curMCLimit)
+    // })
+
     // load the about panel on page load
     pageTitle.innerText = 'About ModReg@EduRec'
     modSelectPanel.style.display = 'block'
@@ -94,5 +108,63 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Appeals clicked')
         pageTitle.innerText = 'Submit Appeals/Inquiries'
         switchPanel(7)
+    })
+
+    addClassBtn.addEventListener('click', () => {
+        console.log('Adding class')
+        modSelPopup.style.display = 'block'
+    })
+
+    modSelPopupClose.addEventListener('click', () => {
+        console.log('Closing popup')
+        modSelPopup.style.display = 'none'
+    })
+
+    function addModToTable(modTitleText, modUnitText, modRankText) {
+        var mod = document.createElement('div')
+        mod.classList += 'mod'
+
+        var modTitle = document.createElement('p')
+        modTitle.innerText = modTitleText
+        modTitle.classList += 'mod-title'
+
+        var modUnit = document.createElement('p')
+        modUnit.innerText = modUnitText
+        modUnit.classList += 'mod-units'
+
+        var modRank = document.createElement('p')
+        modRank.innerText = modRankText
+        modRank.classList += 'mod-rank'
+        
+        var modStatus = document.createElement('p')
+        modStatus.innerText = 'Not Selected'
+        modStatus.classList += 'mod-status'
+        
+        var modEdit = document.createElement('p')
+        modEdit.innerText = 'Edit/View Details'
+        modEdit.classList += 'mod-edit'
+
+        var modRemove = document.createElement('p')
+        modRemove.innerText = 'Remove'
+        modRemove.classList += 'mod-remove'
+
+        mod.appendChild(modTitle)
+        mod.appendChild(modUnit)
+        mod.appendChild(modRank)
+        mod.appendChild(modStatus)
+        mod.appendChild(modEdit)
+        mod.appendChild(modRemove)
+
+        modRemove.addEventListener('click', () => {
+            modContainerTable.removeChild(mod)
+        })
+
+        modContainerTable.appendChild(mod)
+    }
+
+    searchModuleBtn.addEventListener('click', () => {
+        console.log('Closing popup')
+        modSelPopup.style.display = 'none'
+        addModToTable('CS1231 Discrete Structures', 4.00, 3)
     })
 })
